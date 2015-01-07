ab655ad46c965034150f59ae149bfbc180231f8d
Chris MacNaughton <chmacnaughton@gmail.com>
2015-01-07 09:14:25 -0500

    adding metadata?

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'products_chef'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures products_chef'
+long_description 'Installs/Configures products_chef'
+version          '0.1.0'
+
