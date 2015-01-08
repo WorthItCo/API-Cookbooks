2af0651857e1ded28b47fc35b3debcbb3c1c866f
Chris MacNaughton <chmacnaughton@gmail.com>
2015-01-08 16:31:15 -0500

    fix attr type

--- attributes/default/crawler_setup.rb
+++ attributes/default/crawler_setup.rb
@@ -1,1 +1,2 @@
 default[:products_chef][:crawlers] = { 'products.api.worthit.co' => 1 }
+default[:products_chef][:frys_crawlers] = 2
--- recipes/frys_crawler_setup.rb
+++ recipes/frys_crawler_setup.rb
@@ -20,15 +20,12 @@ node[:deploy].each do |application, deploy|
   settings = node[:frys_crawler][application]
   # configure rails_env in case of non-rails app
   rack_env = deploy[:rails_env] || settings[:rack_env] || settings[:rails_env]
-  settings[:frys_crawlers].each do |quantity|
-
-    quantity.times do |idx|
-      idx = idx + 1 # make index 1-based
-      template "/etc/init/frys-crawler-#{application}-#{idx}.conf" do
-        source "site-crawler-n.conf.erb"
-        mode '0644'
-        variables application: application, site: 'frys', rack_env: rack_env, deploy: deploy, instance: idx
-      end
+  settings[:frys_crawlers].times do |idx|
+    idx = idx + 1 # make index 1-based
+    template "/etc/init/frys-crawler-#{application}-#{idx}.conf" do
+      source "site-crawler-n.conf.erb"
+      mode '0644'
+      variables application: application, site: 'frys', rack_env: rack_env, deploy: deploy, instance: idx
     end
   end
 end
